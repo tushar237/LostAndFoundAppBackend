6167c78de621fb998a8a39dd4e2b1f39bbec8941
unknown <chetan.gitm@gmail.com>
2018-04-08 23:33:00 +0530

    initial commit before pull 08Apr2018_1132PM

--- src/main/webapp/LostAndFoundApp/src/app/app.component.ts
+++ src/main/webapp/LostAndFoundApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   tabIndex:number= 0;
+  lostFormData:any={};
+  selectedCategory:number=0;
+  name:String="";
+  emailId:String="";
+  phone:String="";
+  attachment:String="";
+  description:String="";
   constructor(
   ){
 	  
@@ -15,7 +23,31 @@ export class AppComponent {
   
   changeTab(index)
 	  { 
-		 this.tabIndex=index; 
+		 this.tabIndex=index;
+         this.selectedCategory=index; 		 
 		 //alert(this.tabIndex);
 	  }
+	  
+  onSubmit()
+  {
+	  /*
+	  {
+	"createdOn": "07-04-2018",
+	"data": {
+		"category": "Lost",
+		"name": "Chetan Chauhan",
+		"email": "chetan.gitm@gmail.com",
+		"attachment": "aadskjshjksahkjsahkjsakjashjkaskjdsa",
+		"description": "Fossil watch is lost",
+		"lostRcvDate": "07-04-2018"
+	   }
+      }
+	  */
+	  var data={"category":this.selectedCategory,"name":this.name,"email":this.email,"attachment":this.attachment,
+	  "description":this.description,"lostRcvDate":"08-04-2018"};
+	  this.lostFormData={"createdOn":new Date().toString(),"data":data};
+	  console.log(JSON.stringify(this.lostFormData));
+  }
+	  
+	  
 }
